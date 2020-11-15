503e98c6f2e7f7a80ed5a35943c8e40ced315fe8
Muathye <muath.ye@gmail.com>
2020-11-15 05:30:07 +0300

    +add

---
+++ lib/bootstrap/classes.dart
@@ -0,0 +1,23 @@
+// ByMuathye; This will include all project files here to load all classes in one place
+
+// config
+export '../config/assets.dart';
+export '../config/sizes.dart';
+export '../config/styles.dart';
+export '../config/colors.dart';
+export '../config/themes.dart';
+
+// pages
+export '../ui/pages/book/main.dart';
+export '../ui/pages/home/main.dart';
+export '../ui/pages/profile/mian.dart';
+
+// widgets
+export '../ui/widgets/book/pdf_view.dart';
+export '../ui/widgets/book/pages_index.dart';
+export '../ui/widgets/book/favorite_toggle.dart';
+export '../ui/widgets/book/share_current_page.dart';
+export '../ui/widgets/book/jumpToLastPage.dart';
+export '../ui/widgets/book/jumpToPreviousPage.dart';
+export '../ui/widgets/book/jumpToNextPage.dart';
+export '../ui/widgets/book/jumpToFirstPage.dart';
