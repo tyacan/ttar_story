e9c9c7ab360d88fdff92c08296a60c63f6fa8d75
Muathye <muath.ye@gmail.com>
2020-11-15 05:30:33 +0300

    +add

---
+++ lib/bootstrap/packages.dart
@@ -0,0 +1,8 @@
+// ByMuathye; This will include all packages files here to load all classes in one place
+//packages
+export 'package:google_fonts/google_fonts.dart';
+export 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+export 'package:bottom_sheet/bottom_sheet.dart';
+export 'package:shared_preferences/shared_preferences.dart';
+export 'package:native_pdf_view/native_pdf_view.dart';
+export 'package:font_awesome_flutter/font_awesome_flutter.dart';
