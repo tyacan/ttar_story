ef3243da7aa01a4e3a0962f7a696c2693bf7112f
Muathye <muath.ye@gmail.com>
2020-11-15 05:32:03 +0300

    +add

---
+++ lib/core/utilities/shared_preferences.dart
@@ -0,0 +1,87 @@
+//MyMuathye;
+import './../../bootstrap/loader.dart';
+
+class MySharedPreferencesState extends State<MySharedPreferences> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+
+  setStringListValue(String key, List<String> value) async {
+    SharedPreferences myPrefs = await SharedPreferences.getInstance();
+    myPrefs.setStringList(key, value);
+  }
+ 
+  Future<String> getStringListValue(String key) async {
+    SharedPreferences myPrefs = await SharedPreferences.getInstance();
+    return myPrefs.getStringList(key) ?? "";
+  }
+ 
+}
+
+class MySharedPreferences extends StatefulWidget{
+  MySharedPreferences._privateConstructor();
+ 
+  static final MySharedPreferences instance =
+      MySharedPreferences._privateConstructor();
+ 
+  setStringListValue(String key, List<String> value) async {
+    SharedPreferences myPrefs = await SharedPreferences.getInstance();
+    myPrefs.setStringList(key, value);
+  }
+ 
+  Future<String> getStringListValue(String key) async {
+    SharedPreferences myPrefs = await SharedPreferences.getInstance();
+    return myPrefs.getStringList(key) ?? "";
+  }
+ 
+  setStringValue(String key, String value) async {
+    SharedPreferences myPrefs = await SharedPreferences.getInstance();
+    myPrefs.setString(key, value);
+  }
+ 
+  Future<String> getStringValue(String key) async {
+    SharedPreferences myPrefs = await SharedPreferences.getInstance();
+    return myPrefs.getString(key) ?? "";
+  }
+ 
+  setIntegerValue(String key, int value) async {
+    SharedPreferences myPrefs = await SharedPreferences.getInstance();
+    myPrefs.setInt(key, value);
+  }
+ 
+  Future<int> getIntegerValue(String key) async {
+    SharedPreferences myPrefs = await SharedPreferences.getInstance();
+    return myPrefs.getInt(key) ?? 0;
+  }
+ 
+  setBooleanValue(String key, bool value) async {
+    SharedPreferences myPrefs = await SharedPreferences.getInstance();
+    myPrefs.setBool(key, value);
+  }
+ 
+  Future<bool> getBooleanValue(String key) async {
+    SharedPreferences myPrefs = await SharedPreferences.getInstance();
+    return myPrefs.getBool(key) ?? false;
+  }
+ 
+  Future<bool> containsKey(String key) async {
+    SharedPreferences myPrefs = await SharedPreferences.getInstance();
+    return myPrefs.containsKey(key);
+  }
+ 
+  removeValue(String key) async {
+    SharedPreferences myPrefs = await SharedPreferences.getInstance();
+    return myPrefs.remove(key);
+  }
+ 
+  removeAll() async{
+    SharedPreferences myPrefs = await SharedPreferences.getInstance();
+    return myPrefs.clear();
+  }
+
+ @override
+  MySharedPreferencesState createState() => MySharedPreferencesState();
+}
